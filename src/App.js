be288af71df774a06ae2977832b94833e56b930a
asathiya007 <akshay.sathiya@gmail.com>
2019-10-27 01:28:21 -0400

    created routing between pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/Navigation/Navigation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super(); 
+    this.state = {
+      route: "signin"
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route});
+  }
+
+  render() {
+    const {route} = this.state; 
+
+    return (
+      <div>
+        <Navigation route={route} onRouteChange={this.onRouteChange}/>
+      </div>
+    );
+  }
 }
 
 export default App;
